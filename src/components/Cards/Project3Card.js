5be5c21902fe7417d5a96244976e856faf1c136f
shakofa <shakofa.mahmody@gmail.com>
2021-06-20 17:35:21 -0700

    Added project 3 to my portfolio

--- src/components/Cards/Project3Card.js
+++ src/components/Cards/Project3Card.js
@@ -5,30 +5,31 @@ import "./style.css";
 export default function Project3Card() {
     return (
         <div>
-             <Card style={{ width: '55rem' }} className="card">
+            <Card style={{ width: '55rem' }} className="card">
                                 
-                                <Card.Body>
-                                    <Card.Title>3-Bandstand</Card.Title>
-                                    <Card.Text>
-                                    This is our project 3. it is underconstruction and will be added here soon.
-                                    </Card.Text>
-                                    <div>
+                <Card.Body>
+                    <Card.Title>3-Bandstand</Card.Title>
+                    <Card.Text>
+                        This is a Mern stack app that utilizes socket io and many other thechnologies to run a functional, responsive chatroom application. This chatroom app is build for Spotify users who are using spotify prime to listen to music. The users are able chat at the same time they are listing to spotify music.
+                    </Card.Text>
+                    <div>
                                     
-                                    <img className="Image" src={process.env.PUBLIC_URL + "/pro3.png"} width={800} height={500}  alt="img"/>
+                        <img className="Image" src={process.env.PUBLIC_URL + "/pro3.png"} width={800} height={500}  alt="img"/>
                                     
-                                    </div>
-                                    <br />
+                    </div>
+                    <br />
                                     
-                                </Card.Body>
-                                <div>
-                                    <Button className="btn-info">Github Repository</Button>
-                                  
-                                    </div>
-                                    <br />
-                                    <div>
-                                    <Button className="btn-info">Deployed Link</Button>
-                                    </div>
-                                </Card>
+                </Card.Body>
+                    <div>
+                        <a href="https://github.com/shakofa/Bandstand/tree/main"><Button className="btn-info">Github Repository</Button></a>
+                    </div>
+                    <br />
+
+                    <div>
+                        <a href="https://bandstandv1.herokuapp.com/"><Button className="btn-info">Deployed Link</Button></a>
+                                    
+                    </div>
+            </Card>
         </div>
     )
 }
